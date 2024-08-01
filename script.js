316b1f4f1f4fc1e3a9cca16c10e5b99638a61abf
syluuu <syleshkumar2005@gmail.com>
2024-08-01 21:20:52 +0530

    kjhg

---
+++ script.js
@@ -0,0 +1,28 @@
+var skills = ["html", "css", "javascript", "python", "flask","mongo"];
+function addClass(skill) {
+  document.querySelector("."+skill).classList.add(skill+"-anim")
+}
+const removeClass = (skill) => {
+  document.querySelector("."+skill).classList.remove(skill+"-anim")
+}
+function progress() {
+  if (document.getElementById("portanim").offsetTop<=window.pageYOffset && document.getElementById("portfolio").offsetTop>=window.pageYOffset) {
+    for (let i=0;i<skills.length;i++) {
+      addClass(skills[i]);
+    }
+  } else {
+    if (document.getElementById("education").offsetTop>=window.pageYOffset || document.getElementById("portfolio").offsetTop<=window.pageYOffset) {
+      for (let i=0;i<skills.length;i++) {
+        removeClass(skills[i]);
+      }
+    }
+  }
+}
+window.addEventListener("scroll", progress)
+
+const scrollToTop = () => {
+  window.scrollTo(0,0)
+  // document.querySelector(".scroll-top").style.bottom="none"
+  // document.querySelector(".scroll-top").style.top="-10vw"
+}
+document.querySelector(".scroll-top").addEventListener("click", ()=>window.scrollTo(0,0))
